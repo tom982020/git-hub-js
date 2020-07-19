c8d6180aa8c47d45f8e36928fe2dab0e5348a867
Tom982020 <bastom20@gmail.com>
2020-07-19 19:14:12 +0700

    Say Hi Dog

--- Dog.js
+++ Dog.js
@@ -1,8 +1,14 @@
-function Dog() {
-    var name;
+var chalk = require('chalk');
+
+function Dog(name) {
+    this.name = name;
     this.stomach = [];
 }
 
 Dog.prototype.eat = function(cat) {
     this.stomach.push(cat);
-}
+};
+Dog.prototype.sayHi = function() {
+    console.log("Hi! I'm A dog" + chalk.blue(this.name));
+};
+module.exports = Dog;
--- Mouse.js
+++ Mouse.js
@@ -1,4 +1,7 @@
 function Mouse(name) {
     this.name = name;
 }
+Mouse.prototype.die = function() {
+    this.isDead = true;
+}
 module.exports = Mouse;
